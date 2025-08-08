3116774e22bcd6bfa07a18347490974f5535170f
Sonara <marksonara@gmail.com>
2025-08-08 14:43:16 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex-center h-[100vh]">
+      <h1 className="text-xl text-indigo-300">Hello Gsap!</h1>
+    </div>
+  );
+};
+
+export default App;
